22519546bc97783e3077d5fce051adf152a5a755
tannerleroy <tanner@tleroy.com>
2022-05-09 18:39:14 -0400

    text changes in HomeComponent

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <>
-    <div>This is the homepage!</div>
-    <p>This paragraph will give more info about our website and how to use it.</p>
-    <img src={"https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg"} hegiht={700} width={900} />
+    <h1>Adventure Adviser</h1>
+    <h2></h2>
+    <p>Our planet has so much to offer. Looking to plan something fun? Checkout our recommended adventures. If you have an adventure you would like to recommend, please add it via the adventure submit section! </p>
+    <img src={"https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg"} hegiht={600} width={800} />
     </>
   )
 }
