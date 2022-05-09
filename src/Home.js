a5884cbe41e9dba48a9348a9cf33848c2d3c5218
tannerleroy <tanner@tleroy.com>
2022-05-09 17:16:08 -0400

    HomePage text

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <>
     <div>This is the homepage!</div>
     <p>This paragraph will give more info about our website and how to use it.</p>
-    <img src={"https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg"} />
+    <img src={"https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg"} hegiht={700} width={900} />
     </>
   )
 }
