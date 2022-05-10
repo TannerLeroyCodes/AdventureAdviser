9cbd61ad72b6d8275007cd5bcea9580eb42aae8d
Waqas Anjum <Qanjum96@gmail.com>
2022-05-09 22:14:00 -0400

    added initial setup for form

--- src/AdventureSubmit.js
+++ src/AdventureSubmit.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function AdventureSubmit() {
 
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
+  const [location, setLocation] = useState("")
+  const [moreInfo, setMoreInfo] = useState("")
 
-
-
-  
   return (
-    <div>This is the Page for submitting a recomendation of your own</div>
+    <form>
+    <label for="fname">Enter title here</label>
+    <input type="text" id="fname" name="fname" value="John"/>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname" value="Doe"/>
+    </form>
+
   )
 }
 
-export default AdventureSubmit
+export default AdventureSubmit;
