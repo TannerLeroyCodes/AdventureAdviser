158734cadba21b1608dd575232728e71e513c968
tannerleroy <tanner@tleroy.com>
2022-05-10 09:34:01 -0400

    added delete button to new tiles

--- src/Adventure.js
+++ src/Adventure.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 function Adventure({adventure}) {
+
+function handleClick(){
+    fetch(`http://localhost:8000/adventures/${adventure.id}`, {"method": "delete"});
+
+}
+
   return (
-      <li className={"card"}>
-    <div>Title: {adventure.title}</div>
+      <li className={"card"} isNew={adventure.isNew}>
+    <div className={"adventureTitles"}> {adventure.title}</div>
     <img src={adventure.image} className={"image"} height={400} width={550}></img>
     <div>Location: {adventure.location}</div>
     <div> {adventure.moreInfo}</div>
+    {adventure.isNew ? <button onClick={handleClick}>Delete</button> : null }
     </li>
   )
 }
