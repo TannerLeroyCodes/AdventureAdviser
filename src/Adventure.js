7efb9575be7118272504eb6b52224d9c07409c08
tannerleroy <tanner@tleroy.com>
2022-05-11 13:57:00 -0400

    changred adventures from lis to divs for style

--- src/Adventure.js
+++ src/Adventure.js
@@ -8,13 +8,13 @@ function handleClick(){
 }
 
   return (
-      <li className={"card"} >
+      <div className={"card"} >
     <div className={"adventureTitles"}> {adventure.title}</div>
     <img src={adventure.image} className={"image"} height={400} width={550}></img>
     <div>Location: {adventure.location}</div>
     <div> {adventure.moreInfo}</div>
     {adventure.isNew ? <button onClick={handleClick}>Delete</button> : null }
-    </li>
+    </div>
   )
 }
 
